20fbfbed5bc52c9c41ac687025accfd03a2fc029
jackeyge <gexyong@163.com>
2018-09-13 14:07:59 +0800

    add some test

--- tests/authentication_tests.py
+++ tests/authentication_tests.py
@@ -16,5 +16,15 @@ class StandAloneTests(TestCase):
 		    "netease|password\n"
 		self.assertTrue(auth.login('netease', 'password'))
 	
-#	@patch('__builtin__.open')
-	
+	@patch('__builtin__.open')
+	def test_login_bad_creds(self, mock_open):
+		"""错误传参测试"""
+		mock_open.return_value.read.return_value = \
+		    "netease|password"
+		self.assertFalse(auth.login('netease', 'badpassword'))
+		
+	@patch ('__builtin__.open')
+	def test_login_error(self, mock_open):
+		"""方法异常的测试"""
+		mock_open.side_effect = IOError()
+		self.assertFalse(auth.login('netease', 'password'))
